1abda03268df5715e64f6fe9942961cedf9ef7a4
GDR LLC <38796477+gdrone@users.noreply.github.com>
2021-01-18 21:56:04 +0200

    Create Jquery.autoResize.js

---
+++ Jquery.autoResize.js
@@ -0,0 +1,19 @@
+/**
+ * Jquery Plugin
+ * Copyright: Global Development Room, LLC.
+ * Author: Oleksandr Drozd
+*/
+
+$.fn.autoResize = function(){
+	let r = e => {
+		e.style.height = '';
+		e.style.height = e.scrollHeight + 'px'
+	};
+	return this.each((i,e) => {
+		e.style.overflow = 'hidden';
+		r(e);
+		$(e).bind('input', (i,e) => {
+			r(e);
+		})
+	})
+};
